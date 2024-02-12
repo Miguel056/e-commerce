438cf01a8a90907a23c351bc343a829ababfd2aa
Miguel056 <mickey056_lefay@hotmail.com>
2024-02-12 13:15:57 -0600

    Primer commit

---
+++ resources/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Laravel 10 + VUE 3</h1>
+        <hr>
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<script setup>
+const title = "Hola Sahori Te amo <3"
+</script>
+<style scoped>
+div{
+    text-align: center;
+}
+</style>
